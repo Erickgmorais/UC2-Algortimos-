eea58f153b26e8ad5a56a841e7525901ad2edfd6
Erickgmorais <erickk.gustavo.morais@outlook.com>
2026-05-21 20:27:45 -0300

    Add files via upload

---
+++ objeto2.js
@@ -0,0 +1,9 @@
+const people = {
+    name: `Erick`,
+    age: 20,
+    musicGenre: 'rock',
+}
+
+
+console.clear()
+console.log(`The name from the person is ${people.name}, it has ${people.age} years and very like of ${people.musicGenre}`)
