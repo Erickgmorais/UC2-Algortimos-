078988896721353f25cb80d0cff1f124fc4639ed
Erickgmorais <erickk.gustavo.morais@outlook.com>
2026-05-21 20:28:21 -0300

    Delete objeto2.js

--- objeto2.js
+++
@@ -1,9 +0,0 @@
-const people = {
-    name: `Erick`,
-    age: 20,
-    musicGenre: 'rock',
-}
-
-
-console.clear()
-console.log(`The name from the person is ${people.name}, it has ${people.age} years and very like of ${people.musicGenre}`)
